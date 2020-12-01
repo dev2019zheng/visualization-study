1a39fa3c2205e6776f6a7b238ae98c24a9204c8b
dev2019zheng <dev2019zheng@gmail.com>
2020-12-01 11:46:25 +0800

    feat(index): update index page

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,7 +1,3 @@
 import '@babel/polyfill';
-import logoImg from '../../assets/img/bank-pay-logo.png'
-$('.header__logo-title>img').attr('src', logoImg)
 
-$('body')
-  .css('background', '#fff')
-  .css('height', '100vh');
+$('body').css('background', '#fff').css('height', '100vh');
